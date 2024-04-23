8ad0909d0821ae2f040af0218654841f91bf81d9
Thabo Krick <thabo.krick@btc-es.de>
2024-04-23 11:57:45 +0200

    fixed single migration part

--- run_test.py
+++ run_test.py
@@ -1,14 +1,13 @@
-import os
-
 from btc_embedded import migration_source, migration_target
 
-# model files to test
-old_model = os.path.abspath('shc_2020a/Wrapper_seat_heating_control.slx')
-old_script = os.path.abspath('shc_2020a/init_Wrapper_seat_heating_control.m')
+# 2020a model files
+old_model = 'shc_2020a/Wrapper_seat_heating_control.slx'
+old_script = 'shc_2020a/init_Wrapper_seat_heating_control.m'
 
-new_model = os.path.abspath('shc_2023b/Wrapper_seat_heating_control.slx')
-new_script = os.path.abspath('shc_2023b/init_Wrapper_seat_heating_control.m')
+# 2023b model files
+new_model = 'shc_2023b/Wrapper_seat_heating_control.slx'
+new_script = 'shc_2023b/init_Wrapper_seat_heating_control.m'
 
-# perform migration test
+# btc migration test
 btc_project = migration_source(old_model, old_script, '2020a')
-btc_report = migration_target(btc_project, new_model, new_script, '2023b')
+btc_report = migration_target(new_model, new_script, '2023b', epp_file=btc_project)
